849fdc23da11cc561edcc4b272e9d841c70ccee9
ngugimuchangi <tech.curious14@gmail.com>
2023-07-04 17:31:52 +0300

    added component for empty search results

--- src/pages/Home/components/CountryCards.jsx
+++ src/pages/Home/components/CountryCards.jsx
@@ -1,6 +1,7 @@
 import { useAsyncValue } from 'react-router-dom';
 import CountryCard from '../../../components/ui/CountryCard.jsx';
 import useCountries from '../../../hooks/useCountries';
+import NoCountriesFound from '../../../components/ui/NoCountriesFound.jsx';
 
 /**
  * Country cards
@@ -8,13 +9,12 @@ import useCountries from '../../../hooks/useCountries';
 export default function CountryCards() {
   const countries = useAsyncValue();
   const visibleCountries = useCountries(countries);
-
+  if (!visibleCountries.length) return <NoCountriesFound />
   return (
     <div className='cards-section flex flex-row flex-wrap'>
       {
         visibleCountries.map((country, index) => <CountryCard key={index} country={country} />)
       }
     </div>
-
   );
 }
