3f918f156564831aa7d28410c28f7aff28d58c4c
ngugimuchangi <tech.curious14@gmail.com>
2023-07-04 17:27:15 +0300

    specified filter fields in fetch url and change fetch error element

--- src/pages/Country/Country.jsx
+++ src/pages/Country/Country.jsx
@@ -5,8 +5,8 @@ import CountryDetails from './components/CountryDetails.jsx';
 import fetchData from '../../utils/fetchData';
 import formatCountryData from '../../utils/formatCountryData';
 import CountryInfoSkeleton from '../../components/ui/CountryInfoSkeleton.jsx';
-import ErrorMessage from '../../components/ui/ErrorMessage.jsx';
-
+import NoCountriesFound from '../../components/ui/NoCountriesFound.jsx';
+import fields from '../../data/fields.js';
 /**
  * Country page loader. Fetches data for specific country
  * @param {object} - route info
@@ -14,16 +14,16 @@ import ErrorMessage from '../../components/ui/ErrorMessage.jsx';
  */
 export async function loader({ params }) {
   const { alpha } = params;
-  const url = `https://restcountries.com/v3.1/alpha/${alpha}`;
+  const url = `https://restcountries.com/v3.1/alpha/${alpha}?fields=${fields.join(',')}`;
   const data = fetchData(url)
-    .then((country) => formatCountryData(country[0]))
+    .then((country) => formatCountryData(country))
     .then(async (country) => {
       const bordersPromises = [];
       for (const borderAlpha of country.borders) {
-        bordersPromises.push(fetchData(`https://restcountries.com/v3.1/alpha/${borderAlpha}`));
+        bordersPromises.push(fetchData(`https://restcountries.com/v3.1/alpha/${borderAlpha}/?fields=name,cca2`));
       }
       const borders = (await Promise.all(bordersPromises))
-        .map((country) => ({ name: country[0].name.common, alpha: country[0].cca2 }));
+        .map((country) => ({ name: country.name.common, alpha: country.cca2 }));
       country.borders = borders;
       return country;
     });
@@ -45,7 +45,7 @@ export default function Country() {
       <BackButton />
       <div className='country-info flex flex-row flex-wrap justify-space-between'>
         <Suspense fallback={<CountryInfoSkeleton />}>
-          <Await resolve={country} errorElement={<ErrorMessage />}>
+          <Await resolve={country} errorElement={<NoCountriesFound message={'Country not found'} />}>
             <CountryDetails />
           </Await>
         </Suspense>
