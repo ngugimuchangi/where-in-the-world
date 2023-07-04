fdeac4ed67841a9423a59eddb06c7ef9268dbebc
ngugimuchangi <tech.curious14@gmail.com>
2023-07-04 17:24:10 +0300

    adjusted chevron down icon to fixed position irrespective of filter text lenght

--- src/components/ui/Filter.jsx
+++ src/components/ui/Filter.jsx
@@ -3,21 +3,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import FilterOption from './FilterOption.jsx';
 
+/**
+ * Filter list component
+ */
 function Filter() {
   const continents = ['All', 'Africa', 'America', 'Asia', 'Europe', 'Oceania'];
   const defaultText = 'Filter by Region';
   const [filter, setFilter] = useState(defaultText);
   const [filterActive, setFilterActive] = useState(false);
 
+  /**
+   * Toggles filter options visibility
+   */
   function toggleFilters() {
     setFilterActive((isActive) => !isActive);
   }
+
   return (
     <div className=' filter text-body'>
       <div className='filter__active-option'
         onClick={() => toggleFilters()}>
         {filter === 'All' ? defaultText : filter}
-        <FontAwesomeIcon icon={faChevronDown} style={{ marginLeft: '2rem' }} />
+        <FontAwesomeIcon icon={faChevronDown} className='filter-down-arrow' />
       </div>
       <ul className={`filter__list${filterActive ? ' show' : ''}`}>
         {
