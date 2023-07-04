b0aab05c2a7229d466598f670c6a35bb75fb686a
ngugimuchangi <tech.curious14@gmail.com>
2023-07-04 17:23:14 +0300

    added path to link element

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,13 +2,16 @@ import { Link, Outlet } from 'react-router-dom';
 import Container from './Container.jsx';
 import ThemeToggle from '../ui/ThemeToggle.jsx';
 
+/**
+ * Header component
+ */
 function Header() {
   return (
     <>
       <header className='header text-body'>
         <Container>
           <div className='flex flex-row justify-space-between align-center'>
-            <Link>
+            <Link to={'/'}>
               <h1 className='header__title text-body fs-lg fw-900'>Where in the world?</h1>
             </Link>
             <ThemeToggle />
